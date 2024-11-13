205acce96b752dbc04f6276270cc06ebc0595b53
ErinwithBMQ <631165975@qq.com>
2024-11-13 20:53:21 +0800

    11.13

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,8 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className={"bg-blue-200 text-2xl mb-4"}>
+        Welcome to Daily Plan and Diary!
       </div>
       <h1>Vite + React</h1>
       <div className="card">
