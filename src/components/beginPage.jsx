e31bca3ae6846ee355957ad8c8cc895a635284b0
zhangkeyaoo <144611264+zhangkeyaoo@users.noreply.github.com>
2024-11-30 15:50:50 +0800

    begin页面+mian页面框架+diary页面框架

--- src/components/beginPage.jsx
+++ src/components/beginPage.jsx
@@ -1,22 +1,27 @@
-import React, { useState } from 'react';
-import './css/beginPage.css';
+import './css/beginPage.css'
+import React from "react";
 
 function BeginPage() {
     return (
-        <div>
-            <div className={"text-2xl"}>This is the main page</div>
-            <button onClick={() => {
-                window.location.href = `/diary`;
-            }}>diary
-            </button>
-            <button onClick={() => {
-                window.location.href = `/calendar`;
-            }}>calendar
-            </button>
+        <div className="begin-page-container">
+            <div className="begin-page">
+                <div className="header">
+                    <h1 className="site-title">Daily Plan And Diary</h1>
+                </div>
+                <div className="begin-button-container">
+                    
+                    <button onClick={() => {
+                        window.location.href = `/login`;
+                    }}>登录/注册
+                    </button>
+                    <button onClick={() => {
+                        window.location.href = `/mainPage`;
+                    }}>使用指南
+                    </button>
+                </div>
+            </div>
         </div>
     )
-
-
 }
 
-export default BeginPage;
+export default BeginPage
